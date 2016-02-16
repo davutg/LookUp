30fc68f8bc620e67887186f250479bc04851877c
davutg <davutgurbuz@gmail.com>
2016-02-16 23:46:57 +0200

    bower init

--- LookUp/School/wwwroot/js/site.js
+++ LookUp/School/wwwroot/js/site.js
@@ -1,15 +1,18 @@
-﻿
-var element = document.getElementById("userName");
-if (element != null) {
-    element.innerHTML = "Welcome <br>" + element.innerHTML;
-    element.innerHTML += " !";
-}
+﻿/*Avoid of naming collusion , anonym method works outside of global scope*/
+(function (nullMessage) {
+    var element = document.getElementById("userName");
+    if (element != null) {
+        element.innerHTML = "Welcome <br>" + element.innerHTML;
+        element.innerHTML += " !";
 
-element.onmouseover =function()
-    {
-        element.style["color"] = "red";
-}
+        element.onmouseover = function () {
+            element.style["color"] = "red";
+        }
 
-element.onmouseleave = function () {
-    element.style["color"] = "";
-}
+        element.onmouseleave = function () {
+            element.style["color"] = "";
+        }
+    } else {
+        alert(nullMessage);
+    }
+})("element is null")
