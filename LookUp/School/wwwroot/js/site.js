b95db443b602c58f706fff45e607945a3019ae0a
davutg <davutgurbuz@gmail.com>
2016-02-16 23:28:48 +0200

    js init

---
+++ LookUp/School/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿
+var element = document.getElementById("userName");
+if (element != null) {
+    element.innerHTML = "Welcome <br>" + element.innerHTML;
+    element.innerHTML += " !";
+}
+
+element.onmouseover =function()
+    {
+        element.style["color"] = "red";
+}
+
+element.onmouseleave = function () {
+    element.style["color"] = "";
+}
