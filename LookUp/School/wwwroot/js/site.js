4663b22d6471b7bea71ab15087dbdedd4b83815f
davutg <davutgurbuz@gmail.com>
2016-02-17 01:24:30 +0200

    jquery tips added

--- LookUp/School/wwwroot/js/site.js
+++ LookUp/School/wwwroot/js/site.js
@@ -1,18 +1,60 @@
 ﻿/*Avoid of naming collusion , anonym method works outside of global scope*/
+
+$(document).ready(
 (function (nullMessage) {
-    var element = document.getElementById("userName");
+    /* Without JQuery=$
+    var elementX = document.getElementById("userName");
+    elementX.innerHTML = "Welcome <br>" + element.innerHTML;
+    elementX.innerHTML += " !";
+    elementX.onmouseover = function () {
+        element.style["color"] = "red";
+    }
+
+    elementX = function () {
+        element.style["color"] = "";
+    }
+    */
+    var element = $("#userName");
     if (element != null) {
-        element.innerHTML = "Welcome <br>" + element.innerHTML;
-        element.innerHTML += " !";
+        element.text("Welcome \n" + element.text() + " !");
+        element.html(element.html().replace(/\n/g, '<div/>')); //.html('some multiline\ntext').wrap.("<pre />");
 
-        element.onmouseover = function () {
-            element.style["color"] = "red";
-        }
+        element.on("mouseover", function () {
+            element.css('color', 'red');
+        });
 
-        element.onmouseleave = function () {
-            element.style["color"] = "";
-        }
+        element.on("mouseleave", function () {
+            element.css('color', '');
+        });
     } else {
         alert(nullMessage);
     }
-})("element is null")
+
+    var main = $("#main");
+    if (main != null) {
+        main.on("mouseenter", function () {
+            //main.style="color:red;"; ////doesn't work !!!
+            main.css('background-color','#888');
+        });
+        main.on("mouseleave", function () {
+            main.css('background-color', '');
+        });
+    }
+    else {
+        alert(nullMessage);
+    }
+
+    var menuItems = $("ul.menu li a");
+    menuItems.on("click", function () {
+        var me = $(this);
+        alert(me.text());
+    });
+
+})("element is null"));
+
+
+$.fn.multiline = function (txt) {
+    this.text(txt);
+    this.html(this.html().replace(/\n/g, '<br/>'));
+    return this;
+}
