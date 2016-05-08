3469ef96ed99d92fb9a924e0fcfbc155776e585d
Davut GÜRBÜZ <davutgurbuz@gmail.com>
2016-05-08 21:33:43 +0300

    a simple directive coded

---
+++ LookUp/School/wwwroot/js/simpleControls.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    "use strict";
+    angular.module("simpleControls", [])
+    .directive("dcWait", waitControl);
+
+    function waitControl() {
+        return {
+            scope: {
+                show: "=displayWhen"
+            },
+            restrict: "E",
+            templateUrl: "/view/dcWaitTemplate.html"
+        };
+    }
+})();
