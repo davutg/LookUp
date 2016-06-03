f29f144f35b3b6559038d112ff65905ce87438da
Davut Gürbüz <davutg@datasel.com.tr>
2016-06-03 20:14:05 +0300

    classlib+unit test added

---
+++ LookUp/SchoolLib/ParallelTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System;
+
+namespace SchoolLib
+{
+    public class ParallelTests
+    {
+        Dictionary<int, int> tasks = new Dictionary<int, int>();
+        int topNumber = 100000;
+        public TimeSpan LoopNumbersParallel()
+        {
+            var now = DateTime.Now;        
+            var numbers = Enumerable.Range(0, topNumber);
+            var job = from n in numbers.AsParallel()
+                      select n;
+            job.ForAll((x) =>
+            {
+                string info =string.Format("Task number:{0}, value:{1}", System.Threading.Tasks.Task.CurrentId.Value, x);                
+            });
+            return DateTime.Now-now;
+        }
+
+        public TimeSpan LoopNumbers()
+        {
+            var now = DateTime.Now;
+            var numbers = Enumerable.Range(0, topNumber);
+            var job = from n in numbers
+                      select n;
+            foreach (var x in job)
+            {
+                string info = string.Format("Task number:{0}, value:{1}", System.Threading.Tasks.Task.CurrentId.Value, x);
+            }
+
+            return DateTime.Now - now;
+        }
+
+        public static void main(string[] args)
+        {
+            ParallelTests tests = new ParallelTests();
+            tests.LoopNumbersParallel();
+        }
+
+    }
+}
---
+++ LookUp/SchoolTests/ParallelTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SchoolLib;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Diagnostics;
+
+namespace SchoolTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DoesParallelTestWork()
+        {
+            TimeSpan d1=default(TimeSpan), d2=default(TimeSpan);
+            ParallelTests test = new ParallelTests();
+            #region Use of LINQ PARALLEL                                   
+            Action a1=()=>{
+                var m1 = test.LoopNumbersParallel();
+                Debug.WriteLine("parallel" + m1);
+                d1 = m1;
+            };
+
+            Action a2 = () => {
+                var m2 = test.LoopNumbers();
+                Debug.WriteLine("single thread" + m2);
+                d2 = m2;
+            };
+
+            List<Action> tests = new List<Action>() {a1,a2};
+            var testsParallel = tests.AsParallel();
+            Debug.WriteLine(DateTime.Now);
+            testsParallel.ForAll((a) =>
+            {
+                a();
+            });
+            #endregion
+            Assert.IsTrue(d2 > d1);
+            Debug.WriteLine(string.Format("Single:{0},Parallel:{1} ,dif:{2}", d2.Milliseconds, d1.Milliseconds,(d2-d1).Milliseconds));
+        }
+    }
+}
