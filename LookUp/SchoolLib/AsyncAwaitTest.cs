7a68db9ef506b4ac72ef77d2fbc9228829b02972
Davut GÜRBÜZ <davutgurbuz@gmail.com>
2016-06-07 02:11:20 +0300

    Async-Await Test added

---
+++ LookUp/SchoolLib/AsyncAwaitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLib
+{
+    public class AsyncAwaitTest
+    {
+        const int loopCount = 10000;
+        public async Task<bool> DoAnAsyncCall() {
+            bool result= await Task<bool>.Run(() => {
+                for (int i = 0; i < loopCount; i++)
+                {
+                    ((Action)(() => { int x = i ^ 2; Debug.WriteLineIf(x % 4001 == 0, x.ToString()); }))();
+                }                
+                return true;
+            });
+            return result;
+         
+        }
+
+        public bool DoCall()
+        {
+            for (int i = 0; i < loopCount; i++)
+            {
+                ((Action)(() => { int x = i ^ 2; Debug.WriteLineIf(x % 4001 == 0,x.ToString()); }))();                
+            }
+            return true;
+        }         
+    }
+}
---
+++ LookUp/SchoolTests/AsyncAwaitTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SchoolLib;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SchoolTests
+{
+    [TestClass]
+    public class AsyncAwaitTests
+    {
+        [TestMethod]
+        public void DOES_ASYNC_FASTER()
+        {
+            AsyncAwaitTest aaTest = new AsyncAwaitTest();
+            TimeSpan d1 = DateTime.Now.TimeOfDay;
+            TimeSpan d2 = new TimeSpan(d1.Ticks);
+            TimeSpan d3 = new TimeSpan(d1.Ticks);
+            TimeSpan d4 = new TimeSpan(d1.Ticks);
+            Task t1= aaTest.DoAnAsyncCall();
+            d4 = DateTime.Now.TimeOfDay-d4;//Async call is done,assume a state machine created
+            Task.WaitAll(t1);//wait until async Completed
+            d2 = DateTime.Now.TimeOfDay-d2;
+           
+            d1 = DateTime.Now.TimeOfDay;
+            var r2=aaTest.DoCall();
+            d3 = DateTime.Now.TimeOfDay - d1;
+            Debug.WriteLine(String.Format("Started at {0}:\r\nAsync took:{1}ms\r\nSync took:{2}ms\r\nAsync return in:{3}ms",d1,d2.Milliseconds,d3.Milliseconds,d4.Milliseconds));
+
+            Assert.IsTrue(d4 < d2);
+        }
+    }
+}
